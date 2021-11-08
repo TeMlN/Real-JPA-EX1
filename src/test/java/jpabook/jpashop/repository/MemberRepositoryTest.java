f8ef056156eca9146812bebe4b84dc3f24b08a4e
taem1n1 <youman7953@naver.com>
2021-11-08 14:38:44 +0900

    MemberRepository의 Method가 정상작동 하는지 Test

--- src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -1,4 +1,37 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.entity.Member;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class MemberRepositoryTest {
-  
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void testMember() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+        assertThat(savedId).isEqualTo(member.getId());
+        assertThat(findMember.getId()).isEqualTo(savedId);
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member); // 한 Transactional 안에서는 같은 식별자를 가진 Entity는 같은 Entity라 판단한다.
+    }
+
 }
