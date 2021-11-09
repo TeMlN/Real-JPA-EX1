7057d54331a9816e45cbdbd28285e94c74439099
taem1n1 <youman7953@naver.com>
2021-11-09 16:18:07 +0900

    basic entity build

--- src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -1,14 +1,12 @@
 package jpabook.jpashop.repository;
 
-import jpabook.jpashop.entity.Member;
-import org.junit.jupiter.api.Assertions;
+import jpabook.jpashop.domain.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class MemberRepositoryTest {
