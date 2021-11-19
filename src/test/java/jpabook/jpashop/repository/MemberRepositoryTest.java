703916c519dd077b8a53021b6d8362f300142036
taem1n1 <youman7953@naver.com>
2021-11-19 17:21:43 +0900

    회원 서비스 개발

--- src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -23,7 +23,7 @@ class MemberRepositoryTest {
 
         //when
         Long savedId = memberRepository.save(member);
-        Member findMember = memberRepository.find(savedId);
+        Member findMember = memberRepository.findOne(savedId);
 
         //then
         assertThat(savedId).isEqualTo(member.getId());
